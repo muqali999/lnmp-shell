74ae26759392e97cebdbb4a2db580d99e0cbbe5c
tommy <tommy@doitphp.com>
2026-07-20 13:24:11 +0800

    add: valkey安装

---
+++ valkey-install.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    printf "Error: You must be root to run this script!\n"
+    exit 1
+fi
+
+# 定义软件名称和下载网址
+
+#名称
+softwareName="valkey-9.1.0"
+#软件后缀名
+softwareSuffix=".tar.gz"
+
+#下载网址
+downloadUrl="https://github.com/valkey-io/valkey/archive/refs/tags/9.1.0.tar.gz"
+#安装路径
+targetPath="/usr/local/redis"
+
+#校验文件
+verifiedFilePath="/usr/local/redis/bin/redis-server"
+
+printf "\n"
+printf "======================================\n"
+printf " $softwareName Install	    \n"
+printf "======================================\n"
+printf "\n"
+
+if [ ! -s src ]; then    
+    printf "Error: directory src not found.\n"
+    exit 1
+fi
+
+softwareFullName=$softwareName$softwareSuffix
+
+#检测软件是否安装过
+if [ -s $verifiedFilePath ]; then
+    printf "\n$softwareName has been installed.\n\n";
+	exit 1
+fi
+
+groupadd redis
+useradd -g redis redis -s /bin/false
+
+mkdir -p /data/database/redis
+chown -R redis:redis /data/database/redis
+
+mkdir -p /data/logs/redis
+chmod 0777 -R /data/logs/redis
+
+cd src
+
+printf "\n========= source package download start =========\n\n"
+
+if [ -s $softwareFullName ]; then
+    echo "$softwareFullName [found]"
+else
+    echo "$softwareFullName are downloading now..."
+    wget $downloadUrl
+	mv 9.1.0.tar.gz valkey-9.1.0.tar.gz
+fi
+
+if [ -s $softwareName ]; then
+	rm -rf $softwareName
+fi
+tar -zxvf $softwareFullName
+
+printf "\n========= source package download completed =========\n\n"
+
+printf "========= $softwareName install start... =========\n\n"
+
+apt -y install tcl tcl-dev
+
+cd $softwareName
+make
+make test
+
+printf "$softwareName compile success!\n"
+exit 1
+
+make install PREFIX=$targetPath
+
+#生成配置文件
+mkdir -p /usr/local/redis/etc
+
+if [ -s /usr/local/redis/etc/redis.conf ]; then
+    rm /usr/local/redis/etc/redis.conf
+fi
+cp ./valkey.conf /usr/local/redis/etc/redis.conf
+
+cd -
+
+if [ ! -s $verifiedFilePath ]; then
+	printf "Error: $softwareName compile install failed!\n"
+	exit 1
+fi
+
+cd -
+
+#设置配置文件
+if [ ! -d /usr/local/redis/var/run ]; then
+	mkdir -m 0777 -p /usr/local/redis/var/run
+	chown -R redis:redis /usr/local/redis/var/run
+fi
+
+sed -i 's/^daemonize no/daemonize yes/g' /usr/local/redis/etc/redis.conf
+sed -i 's/^dir .\//dir \/data\/redis/g' /usr/local/redis/etc/redis.conf
+sed -i 's/^logfile ""/logfile \/var\/log\/redis\/redislog/g' /usr/local/redis/etc/redis.conf
+sed -i 's/^pidfile \/var\/run\/redis_6379.pid/pidfile \/data\/redis\/var\/run\/redis_6379.pid/g' /usr/local/redis/etc/redis.conf
+
+sed -i 's/^# unixsocket \/run\/redis.sock/unixsocket \/data\/redis\/var\/run\/redis.sock/g' /usr/local/redis/etc/redis.conf
+sed -i 's/^# unixsocketperm 700/unixsocketperm 755/g' /usr/local/redis/etc/redis.conf
+
+isExists=`grep 'vm.overcommit_memory' /etc/sysctl.conf | wc -l`
+if [ "$isExists" != "1" ]; then
+	echo "vm.overcommit_memory = 1">>/etc/sysctl.conf
+	sysctl -p
+fi
+
+#生成随机启机服务文件
+if [ -s /usr/lib/systemd/system/redis.service ]; then
+    rm -rf /usr/lib/systemd/system/redis.service
+fi
+mv ../redis.service.txt /usr/lib/systemd/system/redis.service
+
+systemctl daemon-reload
+systemctl start redis.service
+
+systemctl enable redis.service
+
+ln -s /usr/local/redis/bin/redis-server /usr/bin/redis-server
+ln -s /usr/local/redis/bin/redis-cli /usr/bin/redis
+
+systemctl restart redis.service
+
+ps aux | grep redis | grep -v "grep"
+netstat -ntlp
+
+printf "\n========== $softwareName install end =============\n\n"
+
+printf "============== The End. ==============\n"
